9cadd24e495d315ffc4d55f3a84337552afd96a4
tony555t <your-email@example.com>
2025-10-03 12:50:02 +0300

    useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 // import {useRef} from "react";
 
+// import { useEffect, useRef } from "react";
+
 //   function MyComponent(){
 //     const reference = useRef(true);
 //     console.log(reference.current)
@@ -30,29 +32,57 @@
 // }
 // export default MyComponent;
 
-import { useRef, useState } from "react";
+// import { useRef, useState } from "react";
 
-function MoneyTracker() {
-  const balance = useRef(1000); // stores actual value
-  const [displayBalance, setDisplayBalance] = useState(balance.current); // used for UI
+// function MoneyTracker() {
+//   const balance = useRef(1000); // stores actual value
+//   const [displayBalance, setDisplayBalance] = useState(balance.current); // used for UI
 
-  const handleAddMoney = () => {
-    balance.current += 500;
-    setDisplayBalance(balance.current); // update UI
-  };
+//   const handleAddMoney = () => {
+//     balance.current += 500;
+//     setDisplayBalance(balance.current); // update UI
+//   };
 
-  const handleDeductMoney = () => {
-    balance.current -= 200;
-    setDisplayBalance(balance.current); // update UI
-  };
+//   const handleDeductMoney = () => {
+//     balance.current -= 200;
+//     setDisplayBalance(balance.current); // update UI
+//   };
+
+//   return (
+//     <div>
+//       <h3>Balance: {displayBalance}</h3>
+//       <button onClick={handleAddMoney}>Add 500</button>
+//       <button onClick={handleDeductMoney}>Deduct 200</button>
+//     </div>
+//   );
+// }
+
+// export default MoneyTracker;
+
+import React, { useRef, useEffect } from "react";
+
+function Login() {
+  const usernameRef = useRef(null);
+
+  useEffect(() => {
+    usernameRef.current.focus();
+  }, []);
 
   return (
-    <div>
-      <h3>Balance: {displayBalance}</h3>
-      <button onClick={handleAddMoney}>Add 500</button>
-      <button onClick={handleDeductMoney}>Deduct 200</button>
+    <div style={{ padding: "20px" }}>
+      <h2>Login</h2>
+      <form>
+        <label>
+          Username:{" "}
+          <input type="text" placeholder="Enter username" />
+        </label>
+        <br />
+        <label>
+          Password: <input type="password"ref={usernameRef} placeholder="Enter password" />
+        </label>
+      </form>
     </div>
   );
 }
 
-export default MoneyTracker;
+export default Login;
